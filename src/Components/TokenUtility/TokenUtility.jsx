6f6edca16896fda9043ebd026c22a38b1f913262
Aryan <aryanknight12@gmail.com>
2021-08-01 19:04:03 +0530

    AOS CDN Removed

--- src/Components/TokenUtility/TokenUtility.jsx
+++ src/Components/TokenUtility/TokenUtility.jsx
@@ -6,13 +6,13 @@ export default function TokenUtility() {
     return (
         <div className="token">
             <div className="token-cont">
-                <div className="head-text">
+                <div className="head-text" data-aos="fade-up">
                     <div className="shade-text">UTILITY</div>
                         TOKEN UTILITY
                 </div>
                 <div className="token-cont-1">
 
-                    <div className="token-cont-left">
+                    <div className="token-cont-left" data-aos="fade-up">
                         <div className="token-box">
                             <div className="token-text-box">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit  term 
@@ -26,22 +26,22 @@ export default function TokenUtility() {
 
                     <div className="token-cont-right">
 
-                        <div className="token-head">
+                        <div className="token-head" data-aos="fade-up">
                             Fair launchpad
                         </div>
-                        <div className="token-para">
+                        <div className="token-para" data-aos="fade-up">
                             New startups can showcase their product and raise funds through Initial Dex Offering. The only way to participate in the idos would be by staking MPAD tokens to gain a lottery ticket.
                         </div>
-                        <div className="token-head">
+                        <div className="token-head" data-aos="fade-up">
                             Farming ( Staking ) 
                         </div>
-                        <div className="token-para">
+                        <div className="token-para" data-aos="fade-up">
                             Liquidity providers will be able to generate additional rewards for contributing. Key pools will be incentivized with MPAD tokens. 
                         </div>
-                        <div className="token-head">
+                        <div className="token-head" data-aos="fade-up">
                             Governance
                         </div>
-                        <div className="token-para">
+                        <div className="token-para" data-aos="fade-up">
                             MultiPad community will vote for projects they want to be launched first, changes to lottery modus operandi.
                         </div>
 
