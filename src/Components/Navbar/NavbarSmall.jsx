d618cf48d32f3f392a5d65c862f284d7a8ce3355
Aryan <aryanknight12@gmail.com>
2021-08-01 20:28:40 +0530

    small link added

--- src/Components/Navbar/NavbarSmall.jsx
+++ src/Components/Navbar/NavbarSmall.jsx
@@ -6,9 +6,18 @@ export default function NavbarSmall() {
             <ul className="small-nav-ul">
                 <br />
                 
-                <li style={{cursor: 'pointer'}} className="small-nav-li">Launch App</li>
-                <li style={{cursor: 'pointer'}} className="small-nav-li">Launch your product</li>
-                <li style={{cursor: 'pointer'}} className="small-nav-li">Documentation</li>
+                <li style={{cursor: 'pointer'}} className="small-nav-li">
+                    <a href="#" className="menu-link" target="_blank">Launch App</a>
+                </li>
+                <li style={{cursor: 'pointer'}} className="small-nav-li">
+                    <a href="#" className="menu-link" target="_blank">Launch Your Product</a>
+                </li>
+                <li style={{cursor: 'pointer'}} className="small-nav-li">
+                    <a className="menu-link" href="https://multipad.gitbook.io/multipad/" target="_blank">Documentation</a>
+                </li>
+                <li style={{cursor: 'pointer'}} className="small-nav-li">
+                    <a href="https://multipad.co/77b74b2ad8f9c55e8e05d25520711cc2.pdf" className="menu-link" target="_blank">One Pager</a>
+                </li>
                 {/* <li className="small-nav-li">Token Sale</li>
                 <li className="small-nav-li">Roadmap</li> */}
             </ul>
